2f43d9a8d681fc4b16b3b890dddde8822ce75417
Keshav Gupta <guptakeshav0710@gmail.com>
2024-08-09 21:03:29 +0530

    update model

--- model_building.py
+++ model_building.py
@@ -29,4 +29,5 @@ model.add(Dense(y_train.shape[1], activation="softmax"))
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+
 model.save("emotion_recognition_model.h5")
